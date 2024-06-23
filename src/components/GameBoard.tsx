cd5979bb18c6ae0c005c0b87198b4c8cad3aaa96
John Lundgren <john.lund.af@gmail.com>
2024-06-23 07:20:56 +0200

    Making grid-item clickable only once.

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -3,7 +3,7 @@ import { GameboardComponentProps } from '../types/components.types';
 export function GameBoard({ gameBoard, onTurn }: GameboardComponentProps) {
   return <ol id="gameBoard">
     {gameBoard.map((row, rowIndex) => <li key={rowIndex}><ol>
-      {row.map((col, colIndex) => <li key={colIndex} onClick={() => onTurn(rowIndex, colIndex)}>{col}</li>)}
+      {row.map((col, colIndex) => <li className={col ? 'disabled' : undefined} key={colIndex} onClick={() => { !col && onTurn(rowIndex, colIndex) }}>{col}</li>)}
     </ol></li>)}
   </ol>
 }
